50c7cf573c65336ee3126b039256f7e22ba3c71b
Shreyas Myana <shreyasmyana1997@gmail.com>
2024-03-14 16:12:06 +0530

    ToDo App

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+import Footer from './components/Footer'
+
+function App() {
+
+  return (
+    <div className='h-screen'>
+      <header>
+        <h1 className='text-white'>React With Redux toolkit</h1>
+      </header>
+      <div>
+        <AddTodo />
+        <Todos />
+
+      </div>
+      <footer class="sticky top-[100vh] p-3">
+      <Footer />
+      </footer>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='flex justify-center'>
+        <p className='text-white '>Made With ❤️ By Shreyas</p>
+    </div>
+  )
+}
+
+export default Footer
